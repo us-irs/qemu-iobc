4416a468521a9a2a82996c5e316c353c49b6029f
Maximilian Luz <st111525@stud.uni-stuttgart.de>
2019-10-25 03:32:05 +0200

    arm/isis-obc/at91-pmc: Refactor and cleanup
    Remove generic register storage and improve side-effect handling.

--- hw/arm/isis_obc/at91-pmc.c
+++ hw/arm/isis_obc/at91-pmc.c
@@ -1,75 +1,72 @@
 #include "at91-pmc.h"
 #include "qemu/error-report.h"
 
+#define SO_FREQ        32768        // slow clock oscillator frequency
+#define MO_FREQ     18432000        // main oscillator frequency
 
-enum reg_index {
-    R_PMC_SCER    = 0x00 / 4,
-    R_PMC_SCDR    = 0x04 / 4,
-    R_PMC_SCSR    = 0x08 / 4,
-    R_PMC_PCER    = 0x10 / 4,
-    R_PMC_PCDR    = 0x14 / 4,
-    R_PMC_PCSR    = 0x18 / 4,
-    R_CKGR_MOR    = 0x20 / 4,
-    R_CKGR_MCFR   = 0x24 / 4,
-    R_CKGR_PLLAR  = 0x28 / 4,
-    R_CKGR_PLLBR  = 0x2C / 4,
-    R_PMC_MCKR    = 0x30 / 4,
-    R_PMC_PCK0    = 0x40 / 4,
-    R_PMC_PCK1    = 0x44 / 4,
-    R_PMC_IER     = 0x60 / 4,
-    R_PMC_IDR     = 0x64 / 4,
-    R_PMC_SR      = 0x68 / 4,
-    R_PMC_IMR     = 0x6C / 4,
-    R_PMC_PLLICPR = 0x80 / 4,
-};
+#define SR_MOSCS    0x00000001
+#define SR_LOCKA    0x00000002
+#define SR_LOCKB    0x00000004
+#define SR_MCKRDY   0x00000008
 
-enum reg_access {
-    ACCESS_RESERVED,
-    ACCESS_RW,
-    ACCESS_RO,
-    ACCESS_WO,
-};
+#define PMC_SCER        0x00
+#define PMC_SCDR        0x04
+#define PMC_SCSR        0x08
+#define PMC_PCER        0x10
+#define PMC_PCDR        0x14
+#define PMC_PCSR        0x18
+#define CKGR_MOR        0x20
+#define CKGR_MCFR       0x24
+#define CKGR_PLLAR      0x28
+#define CKGR_PLLBR      0x2C
+#define PMC_MCKR        0x30
+#define PMC_PCK0        0x40
+#define PMC_PCK1        0x44
+#define PMC_IER         0x60
+#define PMC_IDR         0x64
+#define PMC_SR          0x68
+#define PMC_IMR         0x6C
+#define PMC_PLLICPR     0x80
 
-static enum reg_access pmc_reg_access[33] = {
-    ACCESS_WO,
-    ACCESS_WO,
-    ACCESS_RO,
-    ACCESS_RESERVED,
-    ACCESS_WO,
-    ACCESS_WO,
-    ACCESS_RO,
-    ACCESS_RESERVED,
-    ACCESS_RW,
-    ACCESS_RO,
-    ACCESS_RW,
-    ACCESS_RW,
-    ACCESS_RW,
-    ACCESS_RESERVED,
-    ACCESS_RESERVED,
-    ACCESS_RW,
-    ACCESS_RW,
-    ACCESS_RESERVED,
-    ACCESS_RESERVED,
-    ACCESS_RESERVED,
-    ACCESS_RESERVED,
-    ACCESS_RESERVED,
-    ACCESS_RESERVED,
-    ACCESS_WO,
-    ACCESS_WO,
-    ACCESS_RO,
-    ACCESS_RO,
-    ACCESS_RESERVED,
-    ACCESS_RESERVED,
-    ACCESS_RESERVED,
-    ACCESS_RESERVED,
-    ACCESS_RW,
-};
+#define PMC_IRQ_MASK    0x30F
 
+
+static void pmc_update_mckr(PmcState *s)
+{
+    uint8_t css = s->reg_pmc_mckr & 0x03;
+    bool ready = false;
+
+    switch (css) {
+    case 0:     // slow clock
+        ready = true;
+        break;
+
+    case 1:     // main clock
+        ready = s->reg_pmc_sr & SR_MOSCS;
+        break;
+
+    case 2:     // PLLA clock
+        ready = !!(s->reg_pmc_sr & SR_LOCKA);       // clock must be locked/stable
+        ready &= !!(s->reg_ckgr_plla & 0x0000ff);   // non-zero divider
+        ready &= !!(s->reg_ckgr_plla & 0xff0000);   // non-zero multiplier
+        break;
+
+    case 3:     // PLLB clock
+        ready = !!(s->reg_pmc_sr & SR_LOCKB);       // clock must be locked/stable
+        ready &= !!(s->reg_ckgr_pllb & 0x0000ff);   // non-zero divider
+        ready &= !!(s->reg_ckgr_pllb & 0x3f0000);   // non-zero multiplier
+        break;
+    }
+
+    if (ready)
+        s->reg_pmc_sr |= SR_MCKRDY;
+    else
+        s->reg_pmc_sr &= ~SR_MCKRDY;
+}
 
 static uint64_t pmc_mmio_read(void *opaque, hwaddr offset, unsigned size)
 {
     PmcState *s = opaque;
-    unsigned index = offset / 4;
 
     if (size != 0x04) {
         error_report("at91.pmc illegal read access at "
@@ -77,24 +74,54 @@ static uint64_t pmc_mmio_read(void *opaque, hwaddr offset, unsigned size)
         abort();
     }
 
-    if (offset > 0x80) {
-        error_report("at91.pmc illegal read access at 0x%08lx", offset);
-        abort();
-    }
-
-    if (pmc_reg_access[index] == ACCESS_RESERVED || pmc_reg_access[index] == ACCESS_WO) {
-        error_report("at91.pmc illegal read access at 0x%08lx", offset);
-        abort();
-    }
-
     info_report("at91.pmc read 0x%08lx", offset);
-    return s->reg[index];
+
+    switch (offset) {
+    case PMC_SCSR:
+        return s->reg_pmc_scsr;
+
+    case PMC_PCSR:
+        return s->reg_pmc_pcsr;
+
+    case CKGR_MOR:
+        return s->reg_ckgr_mor;
+
+    case CKGR_MCFR:
+        return s->reg_ckgr_mcfr;
+
+    case CKGR_PLLAR:
+        return s->reg_ckgr_plla;
+
+    case CKGR_PLLBR:
+        return s->reg_ckgr_pllb;
+
+    case PMC_MCKR:
+        return s->reg_pmc_mckr;
+
+    case PMC_PCK0:
+        return s->reg_pmc_pck0;
+
+    case PMC_PCK1:
+        return s->reg_pmc_pck1;
+
+    case PMC_SR:
+        return s->reg_pmc_sr;
+
+    case PMC_IMR:
+        return s->reg_pmc_imr;
+
+    case PMC_PLLICPR:
+        return s->reg_pmc_pllicpr;
+
+    default:
+        error_report("at91.pmc illegal read access at 0x%08lx", offset);
+        abort();
+    }
 }
 
 static void pmc_mmio_write(void *opaque, hwaddr offset, uint64_t value, unsigned size)
 {
     PmcState *s = opaque;
-    unsigned index = offset / 4;
 
     if (size != 0x04) {
         error_report("at91.pmc illegal write access at "
@@ -103,43 +130,75 @@ static void pmc_mmio_write(void *opaque, hwaddr offset, uint64_t value, unsigned
         abort();
     }
 
-    if (offset > 0x80) {
-        error_report("at91.pmc illegal write access at "
-                      "0x%08lx [value: 0x%08lx]", offset, value);
-        abort();
-    }
-
-    if (pmc_reg_access[index] == ACCESS_RESERVED || pmc_reg_access[index] == ACCESS_RO) {
-        error_report("at91.pmc illegal write access at "
-                      "0x%08lx [value: 0x%08lx]", offset, value);
-        abort();
-    }
-
     info_report("at91.pmc write 0x%08lx [value: 0x%08lx]", offset, value);
-    s->reg[index] = value;
 
-    // somre register writes have side-effects...
-    switch (index) {
-    case R_CKGR_MOR:    // set/clear MOSCS
-        s->reg[R_PMC_SR] = (s->reg[R_PMC_SR] & ~0x00000001) | (value & 0x00000001);
+    switch (offset) {
+    case PMC_SCER:
+        s->reg_pmc_scsr |= value;
         break;
 
-    case R_CKGR_PLLAR:  // set LOCKA
-        s->reg[R_PMC_SR] = (s->reg[R_PMC_SR] & ~0x00000002) | 0x00000002;
+    case PMC_SCDR:
+        s->reg_pmc_scsr &= ~value;
         break;
 
-    case R_CKGR_PLLBR:  // set LOCKB
-        s->reg[R_PMC_SR] = (s->reg[R_PMC_SR] & ~0x00000004) | 0x00000004;
+    case PMC_PCER:
+        s->reg_pmc_pcsr |= value;
         break;
 
-    case R_PMC_MCKR:    // set MCKRDY
-        s->reg[R_PMC_SR] = (s->reg[R_PMC_SR] & ~0x00000008) | 0x00000008;
+    case PMC_PCDR:
+        s->reg_pmc_pcsr &= ~value;
         break;
+
+    case CKGR_MOR:
+        s->reg_ckgr_mor = value;
+        s->reg_pmc_sr = (s->reg_pmc_sr & ~0x00000001) | (value & 0x00000001);
+        s->reg_ckgr_mcfr = (value & 1) ? (1 << 16) | (MO_FREQ / SO_FREQ / 16) : 0;
+        break;
+
+    case CKGR_PLLAR:
+        s->reg_ckgr_plla = value;
+        s->reg_pmc_sr |= SR_LOCKA;
+        break;
+
+    case CKGR_PLLBR:
+        s->reg_ckgr_pllb = value;
+        s->reg_pmc_sr |= SR_LOCKB;
+        break;
+
+    case PMC_MCKR:
+        s->reg_pmc_mckr = value;
+        break;
+
+    case PMC_PCK0:
+        s->reg_pmc_pck0 = value;
+        break;
+
+    case PMC_PCK1:
+        s->reg_pmc_pck1 = value;
+        break;
+
+    case PMC_IER:
+        s->reg_pmc_imr |= value;
+        break;
+
+    case PMC_IDR:
+        s->reg_pmc_imr &= ~value;
+        break;
+
+    case PMC_PLLICPR:
+        s->reg_pmc_pllicpr = value;
+        break;
+
+    default:
+        error_report("at91.pmc illegal write access at "
+                      "0x%08lx [value: 0x%08lx]", offset, value);
+        abort();
     }
 
-    // TODO: implement missing/re-check existing side-effects
-    // TODO: simulate non-instant change?
-    // TODO: provide interrupts if configured in PMC_IMR
+    pmc_update_mckr(s);
+
+    // set interrupt if requested
+    qemu_set_irq(s->parent_irq, !!(s->reg_pmc_sr & s->reg_pmc_imr & PMC_IRQ_MASK));
 }
 
 static const MemoryRegionOps pmc_mmio_ops = {
@@ -154,27 +213,24 @@ static const MemoryRegionOps pmc_mmio_ops = {
 
 static void pmc_reset_registers(PmcState *s)
 {
-    s->reg[R_PMC_SCSR]    = 0x03;
-    s->reg[R_PMC_PCSR]    = 0x00;
-    s->reg[R_CKGR_MOR]    = 0x00;
-    s->reg[R_CKGR_MCFR]   = 0x00;
-    s->reg[R_CKGR_PLLAR]  = 0x3F00;
-    s->reg[R_CKGR_PLLBR]  = 0x3F00;
-    s->reg[R_PMC_MCKR]    = 0x00;
-    s->reg[R_PMC_PCK0]    = 0x00;
-    s->reg[R_PMC_PCK1]    = 0x00;
-    s->reg[R_PMC_SR]      = 0x08;
-    s->reg[R_PMC_IMR]     = 0x00;
-    s->reg[R_PMC_PLLICPR] = 0x00;
+    s->reg_pmc_scsr    = 0x03;
+    s->reg_pmc_pcsr    = 0x00;
+    s->reg_ckgr_mor    = 0x00;
+    s->reg_ckgr_mcfr   = 0x00;
+    s->reg_ckgr_plla   = 0x3F00;
+    s->reg_ckgr_pllb   = 0x3F00;
+    s->reg_pmc_mckr    = 0x00;
+    s->reg_pmc_pck0    = 0x00;
+    s->reg_pmc_pck1    = 0x00;
+    s->reg_pmc_sr      = 0x08;
+    s->reg_pmc_imr     = 0x00;
+    s->reg_pmc_pllicpr = 0x00;
 }
 
 static void pmc_device_realize(DeviceState *dev, Error **errp)
 {
     PmcState *s = AT91_PMC(dev);
     pmc_reset_registers(s);
-
-    memory_region_init_io(&s->mmio, OBJECT(s), &pmc_mmio_ops, s, "at91.pmc", 0x100);
-    sysbus_init_mmio(SYS_BUS_DEVICE(s), &s->mmio);
 }
 
 static void pmc_device_reset(DeviceState *dev)
@@ -190,10 +246,22 @@ static void pmc_class_init(ObjectClass *klass, void *data)
     dc->reset = pmc_device_reset;
 }
 
+static void pmc_instance_init(Object *obj)
+{
+    SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
+    PmcState *s = AT91_PMC(obj);
+
+    sysbus_init_irq(sbd, &s->parent_irq);
+
+    memory_region_init_io(&s->mmio, OBJECT(s), &pmc_mmio_ops, s, "at91.pmc", 0x100);
+    sysbus_init_mmio(SYS_BUS_DEVICE(s), &s->mmio);
+}
+
 static const TypeInfo pmc_device_info = {
     .name = TYPE_AT91_PMC,
     .parent = TYPE_SYS_BUS_DEVICE,
     .instance_size = sizeof(PmcState),
+    .instance_init = pmc_instance_init,
     .class_init = pmc_class_init,
 };
 
--- hw/arm/isis_obc/at91-pmc.h
+++ hw/arm/isis_obc/at91-pmc.h
@@ -12,7 +12,21 @@
 typedef struct {
     SysBusDevice parent_obj;
     MemoryRegion mmio;
-    uint32_t reg[33];
+    qemu_irq parent_irq;
+
+    // registers
+    uint32_t reg_pmc_scsr;
+    uint32_t reg_pmc_pcsr;
+    uint32_t reg_ckgr_mor;
+    uint32_t reg_ckgr_mcfr;
+    uint32_t reg_ckgr_plla;
+    uint32_t reg_ckgr_pllb;
+    uint32_t reg_pmc_mckr;
+    uint32_t reg_pmc_pck0;
+    uint32_t reg_pmc_pck1;
+    uint32_t reg_pmc_sr;
+    uint32_t reg_pmc_imr;
+    uint32_t reg_pmc_pllicpr;
 } PmcState;
 
 
