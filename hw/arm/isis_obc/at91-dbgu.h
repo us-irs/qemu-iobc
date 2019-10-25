a2d3fa370843e39f83fd9fe4159448a1c29a79ba
Maximilian Luz <st111525@stud.uni-stuttgart.de>
2019-10-25 22:47:01 +0200

    arm/isis-obc: POC implementation for AT91 DBGU

---
+++ hw/arm/isis_obc/at91-dbgu.h
@@ -0,0 +1,34 @@
+#ifndef HW_ARM_ISIS_OBC_DBGU_H
+#define HW_ARM_ISIS_OBC_DBGU_H
+
+#include "qemu/osdep.h"
+#include "hw/sysbus.h"
+#include "chardev/char-fe.h"
+
+
+#define TYPE_AT91_DBGU "at91-dbgu"
+#define AT91_DBGU(obj) OBJECT_CHECK(DbguState, (obj), TYPE_AT91_DBGU)
+
+
+typedef struct {
+    SysBusDevice parent_obj;
+
+    MemoryRegion mmio;
+    CharBackend chr;
+
+    bool rx_enabled;
+    bool tx_enabled;
+
+    // registers
+    uint32_t reg_mr;
+    uint32_t reg_imr;
+    uint32_t reg_sr;
+    uint32_t reg_rhr;
+    uint32_t reg_thr;
+    uint32_t reg_brgr;
+    uint32_t reg_cidr;
+    uint32_t reg_exid;
+    uint32_t reg_fnr;
+} DbguState;
+
+#endif /* HW_ARM_ISIS_OBC_DBGU_H */
