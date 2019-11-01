ca4524130a8eb09f7d30344600075e0796b4e33e
Maximilian Luz <st111525@stud.uni-stuttgart.de>
2019-11-01 14:09:48 +0100

    arm/isis-obc: Implement bootmem bus-matrix support
    Implement minimal bus-matrix support, to enable re-mapping of the
    boot-memory at runtime.
    
    Other features of the bus-matrix are currently not supported.

---
+++ hw/arm/isis_obc/at91-matrix.h
@@ -0,0 +1,41 @@
+#ifndef HW_ARM_ISIS_OBC_MATRIX_H
+#define HW_ARM_ISIS_OBC_MATRIX_H
+
+#include "qemu/osdep.h"
+#include "hw/sysbus.h"
+#include "hw/ptimer.h"
+
+
+#define TYPE_AT91_MATRIX "at91-matrix"
+#define AT91_MATRIX(obj) OBJECT_CHECK(MatrixState, (obj), TYPE_AT91_MATRIX)
+
+typedef enum {
+    AT91_BOOTMEM_ROM,
+    AT91_BOOTMEM_SRAM,
+    AT91_BOOTMEM_SDRAM,
+    __AT91_BOOTMEM_NUM_REGIONS,
+} at91_bootmem_region;
+
+typedef void(at91_bootmem_remap_cb)(void* opaque, at91_bootmem_region target);
+
+
+typedef struct {
+    SysBusDevice parent_obj;
+
+    MemoryRegion mmio;
+
+    uint32_t reg_mrcr;
+
+    at91_bootmem_remap_cb *bootmem_cb;
+    void *bootmem_opaque;
+} MatrixState;
+
+
+inline static void
+at91_matrix_set_bootmem_remap_callback(MatrixState *s, void *opaque, at91_bootmem_remap_cb *cbfn)
+{
+    s->bootmem_cb = cbfn;
+    s->bootmem_opaque = opaque;
+}
+
+#endif /* HW_ARM_ISIS_OBC_MATRIX_H */
