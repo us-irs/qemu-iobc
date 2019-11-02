7f4cf83441e7e8d2af9229765824e3a20f8cd65d
Maximilian Luz <st111525@stud.uni-stuttgart.de>
2019-11-02 20:51:10 +0100

    arm/isis-obc: Add basic LED dummy device

---
+++ hw/arm/isis_obc/gpio-led.c
@@ -0,0 +1,62 @@
+#include "gpio-led.h"
+#include "qemu/error-report.h"
+
+
+static void gpio_led_irq_handle(void *opaque, int n, int level)
+{
+    GpioLedState *s = opaque;
+
+    level = !!level;
+    if (s->state != level) {
+        info_report("led[%s]: state changed to %d", s->name, level);
+        s->state = level;
+    }
+}
+
+static void gpio_led_device_init(Object *obj)
+{
+    GpioLedState *s = GPIO_LED(obj);
+
+    qdev_init_gpio_in_named(DEVICE(s), gpio_led_irq_handle, "led", 1);
+}
+
+static void gpio_led_device_realize(DeviceState *dev, Error **errp)
+{
+    GpioLedState *s = GPIO_LED(dev);
+    s->state = 0;
+}
+
+static void gpio_led_device_reset(DeviceState *dev)
+{
+    GpioLedState *s = GPIO_LED(dev);
+    s->state = 0;
+}
+
+static Property gpio_led_properties[] = {
+    DEFINE_PROP_STRING("name", GpioLedState, name),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+static void gpio_led_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+
+    dc->realize = gpio_led_device_realize;
+    dc->reset = gpio_led_device_reset;
+    dc->props = gpio_led_properties;
+}
+
+static const TypeInfo gpio_led_device_info = {
+    .name = TYPE_GPIO_LED,
+    .parent = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof(GpioLedState),
+    .instance_init = gpio_led_device_init,
+    .class_init = gpio_led_class_init,
+};
+
+static void gpio_led_register_types(void)
+{
+    type_register_static(&gpio_led_device_info);
+}
+
+type_init(gpio_led_register_types)
