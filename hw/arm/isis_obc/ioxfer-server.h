592046f32c0a5d14f4e09aabd4da84d0712d2abd
Maximilian Luz <st111525@stud.uni-stuttgart.de>
2019-12-14 19:19:15 +0100

    arm/isis-obc/iox: Add generic function to send-frame without data

--- hw/arm/isis_obc/ioxfer-server.h
+++ hw/arm/isis_obc/ioxfer-server.h
@@ -56,6 +56,7 @@ static inline uint8_t iox_next_seqid(IoXferServer *srv)
 int iox_send_frame(IoXferServer *srv, struct iox_data_frame *frame);
 int iox_send_data(IoXferServer *srv, uint8_t seq, uint8_t cat, uint8_t id, uint8_t len, uint8_t *data);
 int iox_send_data_multiframe(IoXferServer *srv, uint8_t seq, uint8_t cat, uint8_t id, unsigned len, uint8_t *data);
+int iox_send_command(IoXferServer *srv, uint8_t seq, uint8_t cat, uint8_t id);
 
 int iox_send_u32(IoXferServer *srv, uint8_t seq, uint8_t cat, uint8_t id, uint32_t value);
 
@@ -75,6 +76,11 @@ static inline int iox_send_data_multiframe_new(IoXferServer *srv, uint8_t cat,
                                           uint8_t id, unsigned len, uint8_t *data)
 {
     return iox_send_data_multiframe(srv, iox_next_seqid(srv), cat, id, len, data);
+}
+
+static inline int iox_send_command_new(IoXferServer *srv, uint8_t cat, uint8_t id)
+{
+    return iox_send_command(srv, iox_next_seqid(srv), cat, id);
 }
 
 static inline int iox_send_u32_new(IoXferServer *srv, uint8_t cat, uint8_t id, uint32_t value)
