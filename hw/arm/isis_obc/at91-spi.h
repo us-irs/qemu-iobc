6d95f7ea3e951211b2be095f0da6c9bdafa9fb22
Maximilian Luz <st111525@stud.uni-stuttgart.de>
2020-01-14 19:51:46 +0100

    arm/isis-obc: Implement SPI

--- hw/arm/isis_obc/at91-spi.h
+++ hw/arm/isis_obc/at91-spi.h
@@ -5,11 +5,19 @@
 #include "hw/sysbus.h"
 #include "hw/ptimer.h"
 
+#include "at91-pdc.h"
+#include "ioxfer-server.h"
+
 
 #define TYPE_AT91_SPI "at91-spi"
 #define AT91_SPI(obj) OBJECT_CHECK(SpiState, (obj), TYPE_AT91_SPI)
 
-#define AT91_SPI_NUM_CHANNELS     3
+
+enum wait_rcv_type {
+    AT91_SPI_WAIT_RCV_NONE,
+    AT91_SPI_WAIT_RCV_TDR,
+    AT91_SPI_WAIT_RCV_DMA,
+};
 
 
 typedef struct {
@@ -18,7 +26,31 @@ typedef struct {
     MemoryRegion mmio;
     qemu_irq irq;
 
-    // TODO: registers
+    char* socket;
+    IoXferServer *server;
+    Buffer rcvbuf;
+
+    unsigned mclk;
+
+    uint32_t reg_mr;
+    uint32_t reg_sr;
+    uint32_t reg_imr;
+    uint32_t reg_rdr;
+    uint32_t reg_tdr;
+    uint32_t reg_csr[4];
+
+    uint16_t serializer;
+    bool dma_rx_enabled;
+    bool dma_tx_enabled;
+
+    struct {
+        enum wait_rcv_type ty;
+        uint32_t n;
+    } wait_rcv;
+
+    At91Pdc pdc;
 } SpiState;
+
+void at91_spi_set_master_clock(SpiState *s, unsigned mclk);
 
 #endif /* HW_ARM_ISIS_OBC_SPI_H */
