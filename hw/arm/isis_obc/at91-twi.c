8be5247b1475fe98a3c65010cc3074bca35dba45
Maximilian Luz <st111525@stud.uni-stuttgart.de>
2020-01-28 00:37:58 +0100

    arm/isis-obc/pdc: Fix interrupt-flag handling
    Interrupt flags (ENDRX, ENDTX, RXBUFF, TXBUFE) are only reset on
    RCR/RNCR/TCR/TNCR writes with non-zero values. Additionally, writes with
    zero values to these registers may set the flags.

--- hw/arm/isis_obc/at91-twi.c
+++ hw/arm/isis_obc/at91-twi.c
@@ -613,6 +613,7 @@ static void twi_mmio_write(void *opaque, hwaddr offset, uint64_t value, unsigned
                 .dma_rx_stop  = xfer_dma_rx_stop,
                 .dma_tx_start = xfer_dma_tx_start,
                 .dma_tx_stop  = xfer_dma_tx_stop,
+                .update_irq   = (void (*)(void*))twi_update_irq,
                 .flag_endrx   = SR_ENDRX,
                 .flag_endtx   = SR_ENDTX,
                 .flag_rxbuff  = SR_RXBUFF,
